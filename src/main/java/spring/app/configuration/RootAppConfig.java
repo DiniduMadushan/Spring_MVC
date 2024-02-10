1bddfa16313fc0ee4d74ad1b3172e25f53937880
DiniduMadushan <dinidumadushan15576@gmail.com>
2024-02-10 17:04:55 +0530

    add datasource

--- src/main/java/spring/app/configuration/RootAppConfig.java
+++ src/main/java/spring/app/configuration/RootAppConfig.java
@@ -1,10 +1,50 @@
 package spring.app.configuration;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-
+@EnableTransactionManagement
 public class RootAppConfig {
 
+	@Bean
+	public DataSource datasource() {
+		DriverManagerDataSource datasource=new DriverManagerDataSource();
+		datasource.setDriverClassName("com.mysql.jdbc.Driver");
+		datasource.setUrl("jdbc:mysql://localhost:3306/act_08");
+		datasource.setUsername("testuser");
+		datasource.setPassword("testuser");
+		return datasource;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory=new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(datasource());
+		sessionFactory.setPackagesToScan(new String[] {"spring.app.entity"});
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+	
+	private Properties hibernateProperties() {
+		Properties props=new Properties();
+		props.put("hibernate.dilect", "org.hibernate.dialect.MySQLDialect");
+		return props;
+	}
 
+	@Bean
+	public HibernateTransactionManager getTransactionManager() {
+		HibernateTransactionManager transactionManager=new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory().getObject());
+		return transactionManager;
+	}
 }
